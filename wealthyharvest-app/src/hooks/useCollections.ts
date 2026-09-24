338f43396cc0096dc21c8419a62977b57d227155
PMLLC-create <quintina@programmllc.com>
2026-09-24 15:41:50 -0500

    Fix project folder structure

--- wealthyharvest-app/src/hooks/useCollections.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCollections } from "@/shopify/storefront";
-
-export function useCollections() {
-  return useQuery({
-    queryKey: ["collections"],
-    queryFn: () => getCollections(),
-  });
-}
--- wealthyharvest-app/src/hooks/useProducts.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getProducts } from "@/shopify/storefront";
-
-export function useProducts(options?: { first?: number }) {
-  return useQuery({
-    queryKey: ["products", options?.first ?? 24],
-    queryFn: () => getProducts({ first: options?.first }),
-  });
-}
